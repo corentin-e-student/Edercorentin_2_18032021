7297919a09482e5107133e9b331f29594fde4e47
Corentin Eder <etharion29@yahoo.fr>
2021-03-19 10:54:35 +0100

    First hover in header part

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function myRegsiter() {
+    document.getElementById("register").style.textDecoration="underline";
+}
+
+function myButton() {
+    document.getElementById("button-list").style.color="#0065fc"; border-bottom="1px solid #0065fc";
+}
