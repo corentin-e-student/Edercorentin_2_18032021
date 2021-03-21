f7a5d2c68234f9f22a25672646d64ae099093927
Corentin Eder <etharion29@yahoo.fr>
2021-03-21 15:30:01 +0100

    Modification to read by GitHubpage (1)

--- js/index.js
+++
@@ -1,7 +0,0 @@
-function myRegsiter() {
-    document.getElementById("register").style.textDecoration="underline";
-}
-
-function myButton() {
-    document.getElementById("button-list").style.color="#0065fc"; border-bottom="1px solid #0065fc";
-}
